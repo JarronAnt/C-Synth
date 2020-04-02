3d1fa43a9ab4d3b233309a5bbfe54faf392f802c
SSPYR0 <jarronblizzard@gmail.com>
2020-04-02 16:36:09 -0400

    add more wave forms

--- Sound Synthesizer/main.cpp
+++ Sound Synthesizer/main.cpp
@@ -7,14 +7,52 @@ atomic<double> freqOutput = 0.0;
 double octaveFreq = 110.0; //A2
 double d12thRootOf2 = pow(2.0, 1.0 / 12.0);
 
-double makeNoise(double dtime) {
-	//square wave
-	double x = 1.0 * sin(freqOutput * 2 * PI * dtime) > 0.0 ? 0.2 : -0.2;
-	//sin wave 
-	double s = 1.0 * (sin(freqOutput * 2 * PI * dtime) + sin((freqOutput + 20.0) * 2 * PI * dtime));
+enum type {
+	SINE_WAVE,
+	SQUARE_WAVE,
+	TRIANGLE_WAVE,
+	SAW_WAVE1,
+	SAW_WAVE2,
+	RAND_NOISE
+};
 
-	double t = 1.0 * (sin(freqOutput * 2 * PI * dtime));
-	return t * 0.4;
+
+double w(double hz) {
+	return hz * 2 * PI;
+}
+
+double osc(double hz, double dtime, type t) {
+	switch (t) {
+	case SINE_WAVE:
+		return sin(w(hz) * dtime);
+		break;
+	case SQUARE_WAVE:
+		return sin(w(freqOutput) * dtime) > 0.0 ? 1.0 : -1.0;
+		break;
+	case TRIANGLE_WAVE:
+		return  asin(sin(w(hz) * dtime)) *2.0 / PI;
+		break;
+	case SAW_WAVE1:{
+		double out = 0.0;
+		for (double n = 1.0; n < 100.0; n++) {
+			out += sin(n*w(hz)*dtime) / n;
+		}
+		return out * (2.0 / PI); 
+	}
+		break;
+	case SAW_WAVE2:
+		return (2.0 / PI)*(hz*PI*fmod(dtime, 1.0 / hz) - (PI / 2.0));
+		break;
+	case RAND_NOISE:
+		return 2.0*((double)rand() / (double)RAND_MAX) - 1.0;
+		break;
+	default:
+		return 0.0;
+	}
+}
+
+double makeNoise(double dtime) {
+	return osc(freqOutput, dtime, TRIANGLE_WAVE) * 0.4;
 }
 
 int main() {
@@ -38,7 +76,7 @@ int main() {
 		 keyPressed = false;
 		//map the keyboard to keys on a piano
 		for (int k = 0; k < 16; k++) {
-			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbc\xbe"[k])) & 0x8000){
+			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbc\xbe\xbf"[k])) & 0x8000){
 				if (currKey != k) {
 					freqOutput = octaveFreq * pow(d12thRootOf2, k);
 					wcout << "\rNote On : " << sound.GetTime() << "s " << freqOutput << "Hz";
